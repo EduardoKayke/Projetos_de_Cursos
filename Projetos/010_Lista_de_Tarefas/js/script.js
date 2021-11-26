4ddb14764779fa0d39711249c9d10b11baab820b
eduardokayke <eduardokaykedasilva@gmail.com>
2021-11-26 12:33:35 -0300

    Lista de tarefas.

--- Projetos/010_Lista_de_Tarefas/js/script.js
+++ Projetos/010_Lista_de_Tarefas/js/script.js
@@ -1,7 +1,10 @@
+// Referenciar o input, button e a lista.
 
 let input = document.querySelector('input[name=tarefa');
 let btn = document.querySelector('#botao');
 let lista = document.querySelector('#lista');
+// Usado para os spans de erro. Local da mensagem.
+let card = document.querySelector('.card');
 
 let tarefas = [
     'Jogar GTA V',
@@ -12,13 +15,16 @@ let tarefas = [
 ];
 
 function renderizarTarefas(){
+    //limpar a listagem de itens antes de renderizar novamente a tela.
+    lista.innerHTML = '';
+
     for(tarefa of tarefas){
 
         //Cria o item da lista.
         let itemLista = document.createElement('li');
 
         //Adiciona classes no item da lista.
-        itemLista.setAttribute('class', 'list-group-item list-group-item-action')
+        itemLista.setAttribute('class', 'list-group-item list-group-item-action');
 
         //Cria um texto.
         let itemTexto = document.createTextNode(tarefa);
@@ -28,8 +34,56 @@ function renderizarTarefas(){
 
         //Adicionar o item da lista na lista.
         lista.appendChild(itemLista);
-    }
+    };
 };
 
-//Executando a função para renderizar as tarefas.
-renderizarTarefas();
+// Escutar o evento do botão.
+btn.onclick = function(){
+    // Capturar o valor digitado pelo usuário no input.
+    let novaTarefa = input.value;
+
+    // Para não criar uma lista vazia.
+    if(novaTarefa !== ''){
+        // Atualizar a nova tarefa na lista (array) de tarefas e renderizar a tela.
+        tarefas.push(novaTarefa);
+
+        //Executando a função para renderizar as tarefas.
+        renderizarTarefas(); 
+
+        //Limpar o input. Quando o usuário digitar não fica texto no campo.
+        input.value = '';
+
+        // Remove os spans de erro.
+        removerSpans();
+    }else{
+        // Remove os spans de erro para que não se repitam.
+        removerSpans();
+
+        let span = document.createElement('span');
+        span.setAttribute('class', 'alert alert-warning');
+
+        let msg = document.createTextNode('Você precisa informar a tarefa!');
+
+        span.appendChild(msg);
+
+        card.appendChild(span);
+    };
+};
+
+function removerSpans(){
+    // Recuperamos os spans de erro.
+    let spans = document.querySelectorAll('span');
+    
+    //Varre o array de spans removendo cada um deles.
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    };
+};
+
+
+function main(){
+    //Executando a função para renderizar as tarefas.
+    renderizarTarefas(); 
+};
+
+main();
