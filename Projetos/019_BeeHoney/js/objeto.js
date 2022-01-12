7ae4540c7d6e9108aa67f1a9ef6c4e86381545fe
EduardoKayke <eduardokaykedasilva@gmail.com>
2022-01-12 13:12:57 -0300

    Desenvolvimento BeeHoney curso Udemy parte 3

--- Projetos/019_BeeHoney/js/objeto.js
+++ Projetos/019_BeeHoney/js/objeto.js
@@ -1,5 +1,9 @@
 // Classe pra criar objetos.
 class Obj{
+
+    frame = 1;
+    timer = 0;
+
     constructor(x, y, width, height, color) {
         this.x = x;
         this.y = y;
@@ -7,13 +11,30 @@ class Obj{
         this.height = height;
         this.color = color;
     };
+
     draw() {
-        canvas.fillStyle = this.color;
-        canvas.fillRect(this.x, this.y, this.width, this.height);
+        var img = new Image();
+        img.src = this.color;
+        canvas.drawImage(img, this.x, this.y, this.width, this.height);
+    };
+
+    animation(name, limit) {
+        this.timer += 1;
+
+        if(this.timer > 10) {
+            this.timer = 0;
+            this.frame += 1;
+        };
+
+        if(this.frame > limit) {
+            this.frame = 1;
+        };
+
+        this.color = "assets/" + name + this.frame + ".png"
     };
 };
 
-// extends trás o Obj pra dentro de Bee.
+// Extends trás o Obj pra dentro de Bee.
 // Podemos invocar váriaveis e usar aqui.
 class Bee extends Obj{
     // Direção
@@ -22,14 +43,29 @@ class Bee extends Obj{
     move() {
         this.x += this.dir;
     }
-}
+};
 
 class Spider extends Obj{
     move() {
-        this.y += 2;
+        this.y += 4;
 
         if(this.y > 900) {
             this.y = -50;
-        }
-    }
+            this.x = Math.random() * (400 - 0)
+        };
+    };
+};
+
+class Bg extends Obj{
+    move(speed, limit, newPosition) {
+        this.y += speed;
+    
+        if(this.y > limit) {
+            this.y = newPosition;
+        };
+    };
+};
+
+class Flower extends Spider{
+    // Criado para pegar a movimentação de Math.random de Spider.
 }
--- Projetos/019_BeeHoney/js/script.js
+++ Projetos/019_BeeHoney/js/script.js
@@ -1,8 +1,11 @@
 // Controle do canvas
 var canvas = document.getElementById('canvas').getContext('2d');
 
-var bee = new Bee(200, 500, 100, 100, "yellow");
-var spider = new Spider(100, 100, 100, 100, "black");
+var bg = new Bg(0, 0, 500, 900, "assets/bg.png");
+var flower = new Flower(0, 0, 50, 50, "assets/flower1.png");
+var bg2 = new Bg(0, -900, 500, 900, "assets/bg.png");
+var bee = new Bee(200, 500, 100, 100, "assets/bee1.png");
+var spider = new Spider(100, 100, 100, 100, "assets/spider1.png");
 var spider2 = new Obj(0, 0, 100, 100, "orange");
 
 document.addEventListener('keydown', function(event) {
@@ -23,15 +26,23 @@ document.addEventListener('keyup', function(event) {
 
 // desenhar
 function draw() {
+    bg.draw();
+    bg2.draw();
     bee.draw();
     spider.draw();
-    spider2.draw();
+    flower.draw();
 };
 
 // Atualizar.
 function update() {
+    bg.move(3, 900, 0);
+    bg2.move(3, 0, -900);
     bee.move();
+    bee.animation("bee", 4);
     spider.move();
+    spider.animation("spider", 4);
+    flower.move();
+    flower.animation("flower", 2);
 };
 
 // Receber todas as outras informações.
