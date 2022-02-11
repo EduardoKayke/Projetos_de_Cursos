01ac88cfd3b0f9542babc82588915a3c537f4c2e
EduardoKayke <eduardokaykedasilva@gmail.com>
2022-02-11 18:05:43 -0300

    Node.js aula Projeto

--- Projetos/024_Plataforma_de_Perguntas_e_Respostas_com_MySQL/index.js
+++ Projetos/024_Plataforma_de_Perguntas_e_Respostas_com_MySQL/index.js
@@ -14,19 +14,13 @@
 const express = require("express"); // Importei o módulo.
 const app = express(); // Cópia do Express na variável.
 
-app.get("/", (requisition, response) => {
-
-    // response.send("Bem vindo ao meu site!"); exemplo passado
-
-    response.render("principal/perfil"); //leia mais na parte 3
-
-});
-
-
-
 // ---------------------PARTE 3------------------------------
 // Colocar o EJS no Express.
 app.set('view engine', 'ejs');
+
+// Arquivos estáticos.
+app.use(express.static('public')); 
+
 // PS: Estou dizendo ao Express para usar o EJS como view 
 // engine.
 // Crie uma pasta chamada views, é importante que tenha esse 
@@ -37,7 +31,79 @@ app.set('view engine', 'ejs');
 // Agora que o arquivos está pronto, vamos juntar com Express.
 // Observe o que foi feito na parte 1. 
 
+// ---------------------PARTE 4------------------------------
+app.get("/", (requisition, response) => {
 
+
+    response.render("index");
+
+
+
+    
+    // <!-------PARTE 4 DO INDEX.JS--------->
+    // <!-- <h1>Perguntas e Respostas</h1>
+    // <hr>
+    // <h3>Lista de perguntas</h3>
+    // <img src="/source/img/desenvolvedor.png" alt="Desenvolvedor">
+    // <p>Nome: <%= nome %></p>
+    // <p>Linguagem Favorita: <%= lang %></p>
+    // <p>Empresa: <%= empresa %></p>
+    // <p>Inscritos: <%= inscritos %></p>
+
+    // <% if(msg == true){ %>
+    //     <h3>Isso é uma mensagem de erro!</h3>
+    // <% } else { %>
+    //     <h3>Está tudo em ordem</h3>
+    // <% }; %>
+
+    // <% produtos.forEach(function(produto) { %>
+    // <hr>
+    // <h3><%= produto.nome %></h3>
+    // <h4><%= produto.preco %></h4>
+    // <% }); %> --></hr>
+
+    // Parte de cima foi o INDEX.EJS antigo
+    // Essa de baixo foi aqui com Express.js
+
+    // Vamos linkar as variáveis do JS para o HTML do EJS
+    // No HTML a tag da variável deve ser assim
+    // <%= lang %>, sendo lang qualquer variável.
+    // <%= nome %> e etc...
+    // Para criar uma condição(if, else etc..) é diferente. 
+    // Observe no arquivo index.ejs
+
+    // USADO NO INDEX.EJS
+    // var nome = requisition.params.nome;
+    // var lang = requisition.params.lang;
+    // var exibirMsg = true;
+    // // Vamos usar forEach no EJS veja no arquivo.
+    // var produtos = [
+    //     {nome: "Doritos", preco: 3.14},
+    //     {nome: "Coca-cola", preco: 5},
+    //     {nome: "Leite", preco: 1.45},
+    //     {nome: "Carne", preco: 15},
+    //     {nome: "Redbull", preco: 6},
+    //     {nome: "Nescau", preco: 4},
+    // ];
+
+    // response.render("index", {
+    //     // USADO NO INDEX.EJS
+    //     // nome: nome, 
+    //     // lang: lang,
+    //     // empresa: "Desenvolvedor Freelancer",
+    //     // inscritos: 8000,
+    //     // msg: exibirMsg,
+    //     // produtos: produtos,        
+    // }); //leia mais na parte 3
+
+
+    // response.send("Bem vindo ao meu site!"); exemplo passado antes de criar o response.render("index");
+
+});
+
+app.get("/perguntar", (requisition, response) => {
+    response.render("perguntar");
+});
 
 // ---------------------PARTE 2------------------------------
 // Porta do servidor localhost:8080/
